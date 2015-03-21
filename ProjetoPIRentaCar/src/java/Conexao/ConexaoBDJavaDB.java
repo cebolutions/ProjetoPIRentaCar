a1a62e53085f7f7dd9ebcbbe902575783b20c9c2
cebolutions <ygor_cavalera@hotmail.com>
2015-03-20 22:09:46 -0300

    Projeto JavaWeb

---
+++ ProjetoPIRentaCar/src/java/Conexao/ConexaoBDJavaDB.java
@@ -0,0 +1,38 @@
+
+
+package Conexao;
+
+/**
+ *
+ * @author Fernando
+ */
+public class ConexaoBDJavaDB extends ConexaoBD {
+  
+  private static final String JDBC_DRIVER = "org.apache.derby.jdbc.ClientDataSource";
+  
+  private static final String CONN_STRING = "jdbc:derby://localhost:1527/";
+  
+  private String nomeBanco;
+  
+  public ConexaoBDJavaDB(String nomeBanco) {
+    this.nomeBanco = nomeBanco;
+  }
+
+  @Override
+  protected String getJDBCDriver() {
+    return JDBC_DRIVER;
+  }
+
+  @Override
+  protected String getBancoUrl() {
+    return CONN_STRING + nomeBanco + ";SecurityMechanism=3";
+  }
+
+  @Override
+  protected String[] getCredenciaisAcesso() {
+    String[] credenciais = new String[2];
+    credenciais[0] = "app"; // Nome do usuario do BD
+    credenciais[1] = "app"; // Senha do BD
+    return credenciais;
+  }
+}
